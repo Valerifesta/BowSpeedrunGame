c4c7535a0a63bed26976bdfc98c6c85aa35d73c9
Valerie <victorarsta@gmail.com>
2024-12-18 17:57:03 +0100

    Github commit

---
+++ Assets/Scripts/PortRead.cs
@@ -0,0 +1,127 @@
+using UnityEngine;
+using System.IO.Ports;
+using System;
+using UnityEditor.Experimental.GraphView;
+using System.Runtime.CompilerServices;
+using TMPro;
+using System.Linq;
+using System.Data.Common;
+using System.Net.Configuration;
+
+public class PortRead : MonoBehaviour
+{
+    SerialPort port;
+    public TextMeshProUGUI tmProComp;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void Awake()
+    {
+
+        string existingPort = string.Join("\n", System.IO.Ports.SerialPort.GetPortNames());
+        Debug.Log(existingPort);
+        port = new SerialPort(existingPort, 9600);
+        
+    }
+    void Start()
+    {
+        //TryConnectPort("COM3", 9600);
+        if (port != null)
+        {
+            if (port.IsOpen)
+            {
+                port.Close();
+                Debug.Log("closerd");
+                
+            }
+            port.Open();
+            port.ReadTimeout = 3; //Will time out trying to read if didnt recieve anything after this many miliseconds
+            Debug.Log("opened");
+        }
+        else
+        {
+            Debug.Log("Port is null");
+        }
+        /*
+        try
+        {
+            port.Open();
+            print("opened");
+        }
+        catch
+        {
+            print("Could not open " + "COM3" + " at " + 9600 + " baud rate");
+        }*/
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TryCallbackPing();
+        }
+        TryPortRead();
+    }
+
+    public void TryConnectPort(string comPort, int baudRate)
+    {
+        
+    }
+
+    public void TryCallbackPing()
+    {
+        port.WriteLine("A");
+        //byte[] bytechar = System.Text.Encoding.UTF8.GetBytes("Reset");
+        /*
+        string[] stringByte = new string[bytechar.Length];
+
+        for (int i = 0; i < bytechar.Length; i++)
+        {
+            string a = bytechar[i].ToString();
+            Debug.Log(a);
+            stringByte[i] = a;
+        }
+        string writeString = string.Join("", stringByte);
+        Debug.Log(writeString);
+        */
+
+    }
+    public void TryPortRead()
+    {
+        //bool hasRecieved = false;
+        string read = string.Empty;
+
+        
+
+        try
+        {
+            read = port.ReadLine();
+            tmProComp.text = read;
+            //Debug.Log(read);
+        }
+        catch (TimeoutException a)
+        {
+            //Debug.Log("tmed out");
+        }  
+        /*
+        while (!hasRecieved)
+        {
+            read = port.ReadLine();
+            if (!string.IsNullOrEmpty(read))
+            {
+                hasRecieved = true;
+            }
+        }
+        if (hasRecieved)
+        {
+            print(read);
+            
+        }*/
+    }
+
+    private void OnApplicationQuit()
+    {
+        port.Close();
+    }
+}
