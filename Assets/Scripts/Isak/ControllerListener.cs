ff57933e480b5f0b626f4f7382f3ae2a5d3f9853
izzyn <isak.attila@gmail.com>
2025-01-13 14:43:23 +0100

    Wow

--- Assets/Scripts/Isak/ControllerListener.cs
+++ Assets/Scripts/Isak/ControllerListener.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
 using System.IO.Ports;
 using System;
+using System.Threading;
+using System.Linq;
 
 public class ControllerListener : MonoBehaviour
 {
     SerialPort port;
-    string connectedPortName;
+    string connectedPortName = "/dev/ttyUSB0";
 
     private void Awake()
     {
 
-        connectedPortName = string.Join("\n", System.IO.Ports.SerialPort.GetPortNames());
+        //connectedPortName = string.Join("\n", System.IO.Ports.SerialPort.GetPortNames());
         Debug.Log("Existing ports: " + connectedPortName);
         if (string.IsNullOrEmpty(connectedPortName))
         {
@@ -38,6 +40,8 @@ public class ControllerListener : MonoBehaviour
             port.Open();
             port.ReadTimeout = 5; //Will time out trying to read if didnt recieve anything after this many miliseconds
             Debug.Log("opened");
+            Thread backgroundThread = new Thread(new ThreadStart(ReadFromThread));
+            backgroundThread.Start();
         }
         else
         {
@@ -55,10 +59,88 @@ public class ControllerListener : MonoBehaviour
         }*/
     }
 
-
-    // Update is called once per frame
     void Update()
     {
         
     }
+    void ReadFromThread(){
+      while(true){
+        TryPortRead();
+      }
+    }
+
+    public void TryConnectPort(string comPort, int baudRate)
+    {
+        
+    }
+
+    public void TryCallbackPing()
+    {
+        port.WriteLine("A");
+        //byte[] bytechar = System.Text.Encoding.UTF8.GetBytes("Reset");
+        /*
+        string[] stringByte = new string[bytechar.Length];
+
+        for (int i = 0; i < bytechar.Length; i++)
+        {
+            string a = bytechar[i].ToString();
+            Debug.Log(a);
+            stringByte[i] = a;
+        }
+        string writeString = string.Join("", stringByte);
+        Debug.Log(writeString);
+        */
+
+    }
+    public float ToFloat(Byte[] data){
+      return BitConverter.ToSingle(data, 0);
+
+    }
+    public void TryPortRead()
+    {
+        //bool hasRecieved = false;
+        Byte[] buff = new Byte[32];
+
+
+        try
+        {
+          port.Read(buff, 0, 13);
+          if(buff[0] == 1){
+            Debug.Log(buff);
+            Vector3 list = new Vector3(
+            ToFloat(buff.Skip(1).Take(sizeof(float)).ToArray()),
+            ToFloat(buff.Skip(1+sizeof(float)).Take(sizeof(float)).ToArray()), 
+            ToFloat(buff.Skip(1+2*sizeof(float)).Take(sizeof(float)).ToArray()));
+            Debug.Log(list);
+          }
+        }
+        catch (TimeoutException a)
+        {
+            //Debug.Log("tmed out");
+        }  
+        /*
+        while (!hasRecieved)
+        {
+            read = port.ReadLine();
+            if (!string.IsNullOrEmpty(read))
+            {
+                hasRecieved = true;
+            }
+        }
+        if (hasRecieved)
+        {
+            print(read);
+            
+        }*/
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (port != null)
+        {
+            port.Close();
+            Debug.Log("Closed Port: " + port.PortName);
+        }
+    }
+
 }
