d1c0664d3202fae4436a5c389f0c45cf80bed447
mike wazowski <120007911+Mike-Kowalski@users.noreply.github.com>
2025-01-16 15:53:33 +0100

    wireframe new

---
+++ Assets/Scenes/Test_Scenes/Zion/Zion_Script/ScreenPP.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+public class ScreenPP : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
