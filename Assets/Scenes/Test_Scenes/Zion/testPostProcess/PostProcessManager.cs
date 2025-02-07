3bcc73dc8815293fbf53deac73bbb6b0c4e3a2b9
mike wazowski <120007911+Mike-Kowalski@users.noreply.github.com>
2025-02-07 09:18:38 +0100

    trying to add teleport and shield-filter

--- Assets/Scenes/Test_Scenes/Zion/testPostProcess/PostProcessManager.cs
+++ Assets/Scenes/Test_Scenes/Zion/testPostProcess/PostProcessManager.cs
@@ -6,7 +6,9 @@ using UnityEngine.Rendering.Universal;
 public class PostProcessManager : MonoBehaviour
 {
     private MasterMind mm;
+    private ArrowBehaviour AB;
     private TeleportManager TM;
+    private TestBowBehaviour TBB;
 
     private Volume volume;
     private LiftGammaGain LGG;//gameover and winn effect
@@ -14,13 +16,18 @@ public class PostProcessManager : MonoBehaviour
     private Vignette vignette;//gameover and winn effect
     private WhiteBalance WB;//gameover and winn effect
 
+    private LensDistortion LD;
+    private ShadowsMidtonesHighlights SMH;
+
     //[SerializeField] private AnimationCurve IntensitySmoothStartAnimation;
     //[SerializeField] private AnimationCurve IntensitySmoothLastTimeAnimation;
     //private float IntensitySmoothStart;
     //private float IntensitySmoothLastTime;
 
-  
 
+    // varibles for LensDistortion
+
+    // varibles for ShadowsMidtonesHighlights
 
 
 
@@ -56,7 +63,8 @@ public class PostProcessManager : MonoBehaviour
     void Start()
     {
         mm = FindObjectOfType<MasterMind>();
-
+        AB = FindAnyObjectByType<ArrowBehaviour>();
+        TBB = FindAnyObjectByType<TestBowBehaviour>();
         volume = GetComponent<Volume>();
         volume.profile.TryGet<Vignette>(out vignette);
 
@@ -65,6 +73,10 @@ public class PostProcessManager : MonoBehaviour
         volume.profile.TryGet<ChromaticAberration>(out CA);
 
         volume.profile.TryGet<WhiteBalance>(out WB);
+
+        volume.profile.TryGet<LensDistortion>(out LD);
+        volume.profile.TryGet<ShadowsMidtonesHighlights>(out SMH);
+
 
         RegularFilter();
 
@@ -123,6 +135,63 @@ public class PostProcessManager : MonoBehaviour
        
         CA.intensity.value = 1f;
 
+
+
+
+        if (!isCountingDown)
+        {
+            isCountingDown = true;
+            timeRemaining = duration;
+            Invoke("_tick", 1f);
+        }
+
+    }
+
+    private void ShieldFas()
+    {
+
+        CA.intensity.value = 1f;
+        LGG.gamma.Override(new Vector4(1.30f, 1.44f, 1.25f, 0.347986f));
+        LGG.gain.Override(new Vector4(1.15f, 1.20f, 1.20f, 0.1857141f));
+        LGG.lift.Override(new Vector4(0.83f, 0.71f, 0.94f, -0.14650202f));
+
+        vignette.active = true;
+        vignette.intensity.value = 0.145f;
+        vignette.smoothness.value = 1f;
+        vignette.rounded.value = true;
+        vignette.center.Override(new Vector2(0.5f, 0.5f));
+        vignette.color.Override(new Color(0.1482f, 0.9245f, 0.8956f));
+
+
+        SMH.shadows.Override(new Vector4(2.52f, 2.52f, 2.52f, 0.32234444f));
+        SMH.midtones.Override(new Vector4(4.56f, 4.56f, 4.56f, 0.8351648f));
+        SMH.highlights.Override(new Vector4(1f, 1f, 1f, 1.724992E-09f));
+
+        LGG.active = true;
+        LGG.gamma.Override(new Vector4(0.54f, 0.44f, 0.48f, 1f));
+        LGG.gain.Override(new Vector4(1.86f, 1.90f, 1.90f, 0.9f));
+        LGG.lift.Override(new Vector4(0.89f, 0.79f, 0.98f, -0.2f));
+
+        WB.active = true;
+        WB.tint.value = -0.38f;
+        WB.temperature.value = -100f;
+
+        LD.active = true;
+        LD.center.Override(new Vector2(0.5f, 0.5f));
+        LD.intensity.value = 0.106f;
+        LD.xMultiplier.value = 1f;
+        LD.yMultiplier.value = 1f;
+        LD.scale.value = 1.1f;
+
+
+       
+
+    }
+    private void blueFace()
+    {
+
+        CA.intensity.value = 1f;
+
         if (!isCountingDown)
         {
             isCountingDown = true;
@@ -151,7 +220,7 @@ public class PostProcessManager : MonoBehaviour
     private void RegularFilter()
     {
         vignette.active = true;
-        vignette.intensity.value = 0.119f;//0.319f;
+        vignette.intensity.value = 0.319f;//0.119f;//0.319f;
         vignette.smoothness.value = 1f;
         LGG.gamma.Override(new Vector4(1.54f, 1.42f, 1.46f, -0.09793615f));
         LGG.gain.Override(new Vector4(1.07f, 1.12f, 1.12f, 0.3322843f));
@@ -183,17 +252,21 @@ public class PostProcessManager : MonoBehaviour
     public void Update()
     {
         CheckFilter();
-        if (Input.GetKeyDown(KeyCode.T))
+        if (TBB.tempInputs == false)
         {
             Invoke("TeleportFilter", 0);
         }
+       
+/*
 
-        /*
-         Debug.Log($"Lift W-värde: {LGG.lift.value.w}");
+        Debug.Log($"Lift W-värde: {LGG.lift.value.w}");
         Debug.Log($"Gain W-värde: {LGG.gain.value.w}");
         Debug.Log($"Gamma W-värde: {LGG.gamma.value.w}");
+
+        Debug.Log($"Shadow W-värde: {SMH.shadows.value.w}");
+        Debug.Log($"midtones W-värde: {SMH.midtones.value.w}");
+        Debug.Log($"highlights W-värde: {SMH.highlights.value.w}");
+
         */
-       
-  
     }
 }
